ae4d28937bf91fba05353d256da8f9d17568997b
seongha <11pi885@gmail.com>
2022-11-10 16:13:49 +0900

    feat: server 테스트코드 추가

--- src/communicator/binary/BinaryServerCommunicator.test.ts
+++ src/communicator/binary/BinaryServerCommunicator.test.ts
@@ -29,6 +29,102 @@ describe("BinaryServerCommunicator", () => {
         ]),
       );
     });
+
+    test("send gameInfo to all", () => {
+      const { socketServer, serverSendAllFunction } = createMockSocketServer();
+      const server = new BinaryServerCommunicator(socketServer);
+
+      server.sendAll("gameTurnInfo", {
+        turnSequence: 1,
+        players: [
+          {
+            id: "aaa",
+            nickname: "bbb",
+            score: 2,
+          },
+          {
+            id: "ccc",
+            nickname: "ddd",
+            score: 3,
+          },
+        ],
+        currentPlayerId: "eee",
+        lastWord: "f",
+        deadline: new Date("2022-10-25T07:53:18.728Z"),
+      });
+
+      expect(serverSendAllFunction).toBeCalledTimes(1);
+      expect(serverSendAllFunction).toBeCalledWith(
+        Buffer.from([
+          12, 0, 0, 0, 103, 97, 109, 101, 84, 117, 114, 110, 73, 110, 102, 111, 1, 0, 0, 0, 2, 0, 0, 0, 3, 0, 0, 0, 97,
+          97, 97, 3, 0, 0, 0, 98, 98, 98, 2, 0, 0, 0, 3, 0, 0, 0, 99, 99, 99, 3, 0, 0, 0, 100, 100, 100, 3, 0, 0, 0, 3,
+          0, 0, 0, 101, 101, 101, 1, 0, 0, 0, 102, 24, 0, 0, 0, 50, 48, 50, 50, 45, 49, 48, 45, 50, 53, 84, 48, 55, 58,
+          53, 51, 58, 49, 56, 46, 55, 50, 56, 90,
+        ]),
+      );
+    });
+
+    test("send playerChat to all", () => {
+      const { socketServer, serverSendAllFunction } = createMockSocketServer();
+      const server = new BinaryServerCommunicator(socketServer);
+
+      server.sendAll("playerChat", {
+        playerId: "aaa",
+        nickname: "bbb",
+        content: "ccc",
+      });
+
+      expect(serverSendAllFunction).toBeCalledTimes(1);
+      expect(serverSendAllFunction).toBeCalledWith(
+        Buffer.from([
+          10, 0, 0, 0, 112, 108, 97, 121, 101, 114, 67, 104, 97, 116, 3, 0, 0, 0, 97, 97, 97, 3, 0, 0, 0, 98, 98, 98, 3,
+          0, 0, 0, 99, 99, 99,
+        ]),
+      );
+    });
+
+    test("send systemChat to all", () => {
+      const { socketServer, serverSendAllFunction } = createMockSocketServer();
+      const server = new BinaryServerCommunicator(socketServer);
+
+      server.sendAll("systemChat", {
+        level: "info",
+        content: "aaa",
+      });
+
+      expect(serverSendAllFunction).toBeCalledTimes(1);
+      expect(serverSendAllFunction).toBeCalledWith(
+        Buffer.from([10, 0, 0, 0, 115, 121, 115, 116, 101, 109, 67, 104, 97, 116, 0, 3, 0, 0, 0, 97, 97, 97]),
+      );
+    });
+
+    test("send gameResult to all", () => {
+      const { socketServer, serverSendAllFunction } = createMockSocketServer();
+      const server = new BinaryServerCommunicator(socketServer);
+
+      server.sendAll("gameResult", {
+        players: [
+          {
+            id: "aaa",
+            nickname: "bbb",
+            score: 2,
+          },
+          {
+            id: "ccc",
+            nickname: "ddd",
+            score: 3,
+          },
+        ],
+      });
+
+      expect(serverSendAllFunction).toBeCalledTimes(1);
+      expect(serverSendAllFunction).toBeCalledWith(
+        Buffer.from([
+          10, 0, 0, 0, 103, 97, 109, 101, 82, 101, 115, 117, 108, 116, 2, 0, 0, 0, 3, 0, 0, 0, 97, 97, 97, 3, 0, 0, 0,
+          98, 98, 98, 2, 0, 0, 0, 3, 0, 0, 0, 99, 99, 99, 3, 0, 0, 0, 100, 100, 100, 3, 0, 0, 0,
+        ]),
+      );
+    });
   });
 
   describe("sendOne", () => {
@@ -44,6 +140,21 @@ describe("BinaryServerCommunicator", () => {
         Buffer.from([9, 0, 0, 0, 106, 111, 105, 110, 69, 114, 114, 111, 114, 3, 0, 0, 0, 97, 97, 97]),
       );
     });
+
+    test("send JoinInfo to one", () => {
+      const { socketServer, serverSendOneFunction } = createMockSocketServer();
+      const server = new BinaryServerCommunicator(socketServer);
+
+      server.sendOne("aaa", "joinInfo", { playerId: "aaa", nickname: "bbb" });
+
+      expect(serverSendOneFunction).toBeCalledTimes(1);
+      expect(serverSendOneFunction).toBeCalledWith(
+        "aaa",
+        Buffer.from([
+          8, 0, 0, 0, 106, 111, 105, 110, 73, 110, 102, 111, 3, 0, 0, 0, 97, 97, 97, 3, 0, 0, 0, 98, 98, 98,
+        ]),
+      );
+    });
   });
 
   describe("receive from client", () => {
@@ -57,6 +168,40 @@ describe("BinaryServerCommunicator", () => {
       });
 
       sendToServer("aaa", Buffer.from([4, 0, 0, 0, 106, 111, 105, 110, 3, 0, 0, 0, 98, 98, 98]));
+    });
+
+    test("recieve StartGame command", () => {
+      const { socketServer, sendToServer } = createMockSocketServer();
+      const server = new BinaryServerCommunicator(socketServer);
+
+      server.onReceive("startGame", (id) => {
+        expect(id).toBe("aaa");
+      });
+
+      sendToServer("aaa", Buffer.from([10, 0, 0, 0, 115, 116, 97, 114, 116, 71, 97, 109, 101]));
+    });
+
+    test("recieve Exit command", () => {
+      const { socketServer, sendToServer } = createMockSocketServer();
+      const server = new BinaryServerCommunicator(socketServer);
+
+      server.onReceive("exit", (id) => {
+        expect(id).toBe("aaa");
+      });
+
+      sendToServer("aaa", Buffer.from([4, 0, 0, 0, 101, 120, 105, 116]));
+    });
+
+    test("recieve sendChat command", () => {
+      const { socketServer, sendToServer } = createMockSocketServer();
+      const server = new BinaryServerCommunicator(socketServer);
+
+      server.onReceive("sendChat", (id, data) => {
+        expect(id).toBe("aaa");
+        expect(data).toEqual({ content: "bbb" });
+      });
+
+      sendToServer("aaa", Buffer.from([8, 0, 0, 0, 115, 101, 110, 100, 67, 104, 97, 116, 3, 0, 0, 0, 98, 98, 98]));
     });
   });
 });
