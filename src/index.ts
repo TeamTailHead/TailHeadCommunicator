9fd414b4af44dbf55b24650b93e57df94a839414
Tekiter <tekiter8@gmail.com>
2022-11-15 20:41:56 +0900

    feat: export 추가

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+export { default as BinaryClientCommunicator } from "./communicator/binary/BinaryClientCommunicator";
+export { default as BinaryServerCommunicator } from "./communicator/binary/BinaryServerCommunicator";
 export { default as StringClientCommunicator } from "./communicator/string/StringClientCommunicator";
 export { default as StringServerCommunicator } from "./communicator/string/StringServerCommunicator";
 export { ClientCommunicator, ServerCommunicator } from "./communicator/types";
